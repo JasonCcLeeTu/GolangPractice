27310daa18b634fbfadacda9b89dc275a4c3f007
jason <qqertytt258@gmail.com>
2022-04-25 09:22:53 +0800

    test

--- maze/maze.go
+++ maze/maze.go
@@ -7,12 +7,12 @@ import(
 func main(){
 	 maze :=[][]int{{0,1,0,0,0},{0,0,0,1,0},{0,1,0,1,0},{1,1,1,0,0},{0,1,0,0,1},{0,1,0,0,0}}
 
-	 steps:=make([]int,len(maze[0]))
-	steps := make([][]int, len(maze[0]))
-	for i:=range steps{
-		fmt.Println(steps)
-	 fmt.Println(len(maze[0]))
-	}
+	//  steps:=make([]int,len(maze[0]))
+	// steps := make([][]int, len(maze[0]))
+	// for i:=range steps{
+	// 	fmt.Println(steps)
+	//  fmt.Println(len(maze[0]))
+	// }
 	
 }
 
